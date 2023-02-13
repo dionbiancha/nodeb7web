5f3d287077f4976a5e0278431ac72739479786ff
Dionei Bianchati <dion.biancha@gmail.com>
2023-02-13 17:16:59 -0300

    feat: pagina nao encontrada

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import mainRouts from "./routes";
 import painelRouts from "./routes/painel";
 
 const server = express();
 
 server.use(mainRouts);
+
 server.use("/painel", painelRouts);
+
+server.use((req: Request, res: Response) => {
+  res.status(404).send("Pagina nao encontrada");
+});
 
 server.listen(3000);
