936b7af5e9fc7f960b5237116e831bd190736a64
Dionei Bianchati <dion.biancha@gmail.com>
2023-02-16 20:36:21 -0300

    feat: configuração mustache

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send("hello");
+  res.render("home");
 });
 
 router.get("/contato", (req: Request, res: Response) => {
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,13 @@ import express, { Request, Response } from "express";
 import mainRouts from "./routes";
 import painelRouts from "./routes/painel";
 import path from "path";
+import mustache from "mustache-express";
 
 const server = express();
+
+server.set("view engine", "mustache");
+server.set("views", path.join(__dirname, "views"));
+server.engine("mustache", mustache());
 
 server.use(express.static(path.join(__dirname, "../public")));
 
