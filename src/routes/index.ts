2cb4a49c50ac7f45d844b1213b8ee319e605078c
Dionei Bianchati <dion.biancha@gmail.com>
2023-02-19 19:31:06 -0300

    feat: lista e condicionais

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,23 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  let user: string = "Dionei";
+  let name: string = "Dionei";
+  let age: number = 50;
+  let showOld: boolean = false;
+  let products = [
+    { title: "Produto X", price: 10 },
+    { title: "Produto Y", price: 15 },
+    { title: "Produto Z", price: 20 },
+  ];
+
+  if (age >= 90) {
+    showOld = true;
+  }
+
   res.render("home", {
-    user,
+    name,
+    showOld,
+    products,
   });
 });
 
